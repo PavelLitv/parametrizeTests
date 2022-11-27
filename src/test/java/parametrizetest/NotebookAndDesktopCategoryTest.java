a92fb027c6230cf6ac46f276cbbb87d9fe03c1ce
Pavel Litvinenko <korvine1980@yandex.ru>
2022-11-27 15:12:12 +0500

    add parametrizedTests

---
+++ src/test/java/parametrizetest/NotebookAndDesktopCategoryTest.java
@@ -0,0 +1,73 @@
+package parametrizetest;
+
+import com.codeborne.selenide.CollectionCondition;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Selenide.*;
+
+public class NotebookAndDesktopCategoryTest extends TestBase {
+
+    static Stream<Arguments> verifyAvailableFilter() {
+        return Stream.of(
+                Arguments.of("Ноутбуки", List.of("Срок доставки", "Бренд", "Продавец", "Цена, ₽", "Скидка", "Цвет",
+                        "Назначение ноутбука", "Диагональ экрана", "Тип матрицы", "Разрешение экрана", "Поверхность экрана",
+                        "Операционная система", "Линейка процессоров", "Количество ядер процессора", "Объем оперативной памяти (Гб)",
+                        "Производитель видеопроцессора", "Видеоадаптер", "Объем памяти видеоадаптера", "Оптический привод", "Тип накопителя",
+                        "Объем накопителя HDD", "Объем накопителя SSD", "Процессор")),
+
+                Arguments.of("Компьютеры и моноблоки", List.of("Категория", "Срок доставки", "Бренд", "Продавец", "Цена, ₽",
+                        "Скидка", "Объем оперативной памяти (Гб)", "Объем памяти видеоадаптера", "Операционная система", "Тип видеокарты")),
+
+                Arguments.of("Мониторы", List.of("Срок доставки", "Бренд", "Продавец", "Цена, ₽", "Скидка", "Цвет",
+                        "Диагональ", "Разрешение экрана", "Тип матрицы", "Формат экрана", "Частота обновления", "Вид разъема",
+                        "Поверхность экрана", "Размер VESA", "Тип блока питания")),
+
+                Arguments.of("Периферийные аксессуары", List.of("Категория", "Срок доставки", "Бренд", "Продавец", "Скидка", "Цена, ₽"))
+        );
+    }
+
+    @MethodSource
+    @ParameterizedTest(name = "Проверяем в подкатегории {0} доступные фильтры {1}")
+    @Tag("High")
+    void verifyAvailableFilter(String category, List<String> filters) {
+        open("/catalog/elektronika/noutbuki-periferiya");
+        $(".sidemenu").$(byText(category)).click();
+        $$(".filter__name").filter(visible).shouldHave(CollectionCondition.texts(filters));
+    }
+
+    @CsvSource({
+            "Acer", "lenovo", "Asus", "HP", "Apple", "Infinix", "Xiaomi"
+    })
+
+    @ParameterizedTest(name = "Проверка фильтра ноутбуков по производителю {0}")
+    @Tag("High")
+    void verifyFilterByBrand(String filter) {
+        open("/catalog/elektronika/noutbuki-pereferiya/noutbuki-ultrabuki");
+        $("[data-filter-name=fbrand]").$(withText(filter)).click();
+        $$(".product-card__brand").shouldHave(CollectionCondition.allMatch(
+                "Проверяем, что в карточках товара содержится наименование бренда",
+                (selenideElement -> selenideElement.getText().contains(filter))));
+    }
+
+    @CsvFileSource(resources = "currencies.csv")
+    @ParameterizedTest(name = "Выбираем доступную валюту {0}, проверяем наименование в header {1} и в цене товара {2}")
+    void verifyPriceByCurrencies(String currency, String latinName, String mark){
+        open("/catalog/elektronika/noutbuki-periferiya");
+        $("[data-link=currentCurrency]").hover();
+        $(by("action", "/webapi/setcurrency")).$(byText(currency)).click();
+        $("[data-link=currentCurrency]").shouldHave(text(latinName));
+        $(".price__wrap").shouldHave(text(mark));
+    }
+
+}
---
+++ src/test/java/parametrizetest/TestBase.java
@@ -0,0 +1,14 @@
+package parametrizetest;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+
+public class TestBase {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.baseUrl = "https://www.wildberries.ru";
+        Configuration.browserSize = "1920x1080";
+        Configuration.holdBrowserOpen = true;
+    }
+}
